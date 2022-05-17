477f1642266dbe9c4d27bbf7f24a33d26235102c
Jeremie St-Pierre Robitaille <jeremiesrobitaille@gmail.com>
2022-05-17 14:01:40 -0400

    Fixed var names

--- network.tf
+++ network.tf
@@ -5,8 +5,8 @@ resource "azurerm_virtual_network" "this" {
   name = format("vn-%s-%s-%s",
   local.naming.location[var.location], var.environment, var.project)
 
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   address_space = ["10.0.0.0/16"]
 }
@@ -18,7 +18,7 @@ resource "azurerm_virtual_network" "this" {
 resource "azurerm_subnet" "private" {
   name = "${var.databricks_workspace_name}-snet"
 
-  resource_group_name  = azurerm_resource_group.this.name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.0.0/24"]
 
@@ -38,7 +38,7 @@ resource "azurerm_subnet" "private" {
 
 resource "azurerm_subnet" "public" {
   name                 = "${var.databricks_workspace_name}-pub-snet"
-  resource_group_name  = azurerm_resource_group.this.name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.1.0/24"]
 
@@ -62,8 +62,8 @@ resource "azurerm_subnet" "public" {
 
 resource "azurerm_network_security_group" "private" {
   name                = "${var.databricks_workspace_name}-nsg"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_name.location
 }
 
 resource "azurerm_subnet_network_security_group_association" "private" {
@@ -74,8 +74,8 @@ resource "azurerm_subnet_network_security_group_association" "private" {
 
 resource "azurerm_network_security_group" "public" {
   name                = "${var.databricks_workspace_name}-pub-nsg"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_name.location
 }
 
 resource "azurerm_subnet_network_security_group_association" "public" {
@@ -91,15 +91,15 @@ resource "azurerm_subnet_network_security_group_association" "public" {
 
 resource "azurerm_public_ip" "this" {
   name                = "${var.databricks_workspace_name}-pip"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = var.resource_group_name.location
+  resource_group_name = var.resource_group_name
   allocation_method   = "Static"
 }
 
 resource "azurerm_lb" "this" {
   name                = "${var.databricks_workspace_name}-lb"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = var.resource_group_name.location
+  resource_group_name = var.resource_group_name
 
   frontend_ip_configuration {
     name                 = "DatabricksPublicIPAddress"
