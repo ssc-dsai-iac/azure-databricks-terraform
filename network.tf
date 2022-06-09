21f490439459032889c2c911da86262836fc9d2d
Jeremie St-Pierre Robitaille <jeremiesrobitaille@gmail.com>
2022-06-09 11:38:23 -0400

    Fix loadbalancer id mapping with count'

--- network.tf
+++ network.tf
@@ -129,5 +129,5 @@ resource "azurerm_lb_backend_address_pool" "this" {
 
   name                = "${var.databricks_workspace_name}-bap"
   resource_group_name = var.resource_group_name
-  loadbalancer_id     = azurerm_lb.this.id
+  loadbalancer_id     = azurerm_lb.this[0].id
 }
